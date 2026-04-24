50e6ea4e9e7fd407c69b8904d599224c90c55954
Himanshu <hm839807@gmail.com>
2026-04-23 17:53:39 -0700

    fix leads data file

--- src/Data/leads.js
+++ src/Data/leads.js
@@ -1,1 +1,14 @@
-export const leads = [];
+export const leads = [
+  {
+    id: 1,
+    name: "John Doe",
+    email: "john@example.com",
+    status: "New Lead"
+  },
+  {
+    id: 2,
+    name: "Sarah Smith",
+    email: "sarah@example.com",
+    status: "Contacted"
+  }
+];
