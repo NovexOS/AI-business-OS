4bdfaf3535b41c52a75944368444fbe2b6a8dc05
Himanshu <hm839807@gmail.com>
2026-04-23 17:56:39 -0700

    added leads file

--- src/Data/leads.js
+++ src/Data/leads.js
@@ -1,14 +1,14 @@
 export const leads = [
   {
     id: 1,
-    name: "John Doe",
-    email: "john@example.com",
+    name: "Rahul Sharma",
+    email: "rahul@example.com",
     status: "New Lead"
   },
   {
     id: 2,
-    name: "Sarah Smith",
-    email: "sarah@example.com",
+    name: "Priya Singh",
+    email: "priya@example.com",
     status: "Contacted"
   }
 ];
