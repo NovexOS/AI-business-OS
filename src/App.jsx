ee3d474794316ded8165b525ea1cd491fa66f02a
Himanshu <hm839807@gmail.com>
2026-04-24 11:03:22 -0700

    router added

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import Leads from "./pages/Leads"
-import Home from "./pages/Home"
-import Pricing from "./pages/Pricing"
-import Dashboard from "./pages/Dashboard"
-import Automation from "./pages/Automation"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import Leads from "./pages/Leads";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <BrowserRouter>
-
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/pricing" element={<Pricing />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/leads" element={<Leads />} />
-        <Route path="/automation" element={<Automation />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        
       </Routes>
-
-    </BrowserRouter>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
